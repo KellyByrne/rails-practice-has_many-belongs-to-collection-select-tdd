1101733e4c786cab08f68eb5a12cc39bb849c6d5
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-12-09 10:14:49 -0700

    something

---
+++ spec/features/cats_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature "Cats" do
+	scenario "show should list the toys that cat owns" do
+		cat = Cat.create!(name: "Fluffy")
+		toy = create_toy(cat)
+		visit cat_path(cat)
+		#cats_path index page, new_cat_path new page, cat_path(cat) show page, update also specify cat
+		expect(page).to have_content(toy.name)
+	end
+end
+
+def create_cat
+	Cat.create!(:name => 'Fluffy Boots')
+end
+
+def create_toy(cat)
+	Toy.create!(:name => 'Lazer Pointer', cat_id: cat.id)
+end
+
